8addda4ea282163d2e1126db06a0db8a4a8e59dc
Maaz_Khan <maazkhan0348810@gmail.com>
2023-10-19 04:24:13 +0500

    the program is reverse of the Guessing Game program i made variable and prompt to user(gamer) to take two number

---
+++ src/reverseGuessingGame.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int highestNum, lowestNum, guess;
+    char decision;
+    printf("User give the Two number Highest and lowest number\n");
+    printf("Give the Highest and Lowest Number:");
+    scanf("%d%d",&highestNum,&lowestNum);
+    printf("Pick a number between %d and %d to guess\n",highestNum,lowestNum);
+    
+    
+}
