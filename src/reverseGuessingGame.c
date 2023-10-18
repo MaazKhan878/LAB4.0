c8ebceea1c4a5ac06b303642ce5f8d4c60b2c99b
Maaz_Khan <maazkhan0348810@gmail.com>
2023-10-19 04:39:16 +0500

    using while loop if the user say the number is correct then the loop well terminate if not correct then the loop will repeat again and again

--- src/reverseGuessingGame.c
+++ src/reverseGuessingGame.c
@@ -6,6 +6,24 @@ int main(){
     printf("Give the Highest and Lowest Number:");
     scanf("%d%d",&highestNum,&lowestNum);
     printf("Pick a number between %d and %d to guess\n",highestNum,lowestNum);
-    
+    while(decision!='c'){
+        guess=(highestNum + lowestNum)/3;
+        printf("The guess number as %d\n",guess);
+        printf("If the guess number as correct then press 'c' , Lowest then press 'l' , Highest press 'h':");
+        scanf(" %c",&decision);
+        if(decision=='l'){
+            highestNum-=2;
+        }
+        else if(decision=='h'){
+            lowestNum+=2;
+        }
+        else{
+            printf("Please tell them the number is correct or not\n");
+        }
+        
+        
+    }
+    printf("Congratulation you guess the number %d\n",guess);
+    return 0;
     
 }
